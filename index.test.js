510e01f609df83ee6fe151bef74f77abc402d347
Hashid <hashidlouis@gmail.com>
2024-11-12 14:21:44 -0500

    Routes and Tests updated

--- index.test.js
+++ index.test.js
@@ -13,7 +13,25 @@ const {seedMusician} = require("./seedData");
 describe('./musicians endpoint', () => {
     // Write your tests here
     
+    test("Returns Musician", async () => {
+        const response = await request(app).get("/musicians/2")
+        expect(response.body.id).toBe(2)
+    })
+
+    test("Can create a new Musician", async () => {
+        const response = await request(app).get("/musicians/:name/:intstrument")
+        expect(response.status).toBe(200)
+    })
     
+    test("Can update a Musician", async () => {
+        const response = await request(app).put("/musicians/:ID")
+        expect(response.status).toBe(200)
+    })
+
+    test("Can Delete a Musician", async () => {
+        const response = await request(app).put("/musicians/:ID")
+        expect(response.status).toBe(200)
+    })
 
 
 
--- src/app.js
+++ src/app.js
@@ -5,8 +5,62 @@ const { db } = require("../db/connection")
 
 const port = 3000;
 
-//TODO: Create a GET /musicians route to return all musicians 
+app.use(express.json())
+app.use(express.urlencoded())
 
+
+app.get("/musicians", async (req, res) => {
+    const musicians = await Musician.findAll();
+    res.json(musicians);
+  });
+
+app.get("/musicians/:id", async (req, res) => {
+    const id = req.params.id;
+    const musican = await Musician.findByPk(id);
+    res.json(musican);
+  });
+
+
+app.post("/musicians", async (req, res, next) => {
+    
+    try{
+        const artist =  await Musician.create(req.body);
+        
+
+        res.status(201).json({message: "Musician Created"})
+
+    }
+
+    catch (error) {
+        next (error)
+    }
+})
+
+app.put("/musicians/:id", async (req,res) => {
+
+    try{
+      await Musician.update(req.body, {
+        where: {id : req.params.id}
+      })
+      res.status(200)
+      }
+
+    catch (err)
+    {
+        res.status(404)
+    }
+})
+
+app.delete("/musicians/:id", async(req,res) => {
+    try{
+        await Musician.destroy({where: {id: req.params.id}})
+        res.status(200).message("User Deleted!")
+    }
+    catch(err){
+        res.send(400)
+    }
+
+})
 
 
 
