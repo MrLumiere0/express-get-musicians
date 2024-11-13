6471666d9394bff517bf59886298293b4d83078f
Hashid <hashidlouis@gmail.com>
2024-11-13 09:44:08 -0500

    Added updateed routes and tests

--- index.test.js
+++ index.test.js
@@ -4,32 +4,31 @@ execSync('npm install');
 execSync('npm run seed');
 
 const request = require("supertest")
-const { db } = require('./db/connection');
 const { Musician } = require('./models/index')
-const app = require('./src/app');
 const {seedMusician} = require("./seedData");
+const router = require('./db/routes/musician')
 
 
 describe('./musicians endpoint', () => {
     // Write your tests here
     
     test("Returns Musician", async () => {
-        const response = await request(app).get("/musicians/2")
+        const response = await request(router).get("/musicians/2")
         expect(response.body.id).toBe(2)
     })
 
     test("Can create a new Musician", async () => {
-        const response = await request(app).get("/musicians/:name/:intstrument")
+        const response = await request(router).get("/musicians/:name/:intstrument")
         expect(response.status).toBe(200)
     })
     
     test("Can update a Musician", async () => {
-        const response = await request(app).put("/musicians/:ID")
+        const response = await request(router).put("/musicians/:id")
         expect(response.status).toBe(200)
     })
 
     test("Can Delete a Musician", async () => {
-        const response = await request(app).put("/musicians/:ID")
+        const response = await request(router).put("/musicians/:id")
         expect(response.status).toBe(200)
     })
 
--- src/app.js
+++ src/app.js
@@ -1,68 +1,23 @@
 const express = require("express");
 const app = express();
-const { Musician } = require("../models/index")
-const { db } = require("../db/connection")
-
+const { Musician } = require("../models/index");
+const { db } = require("../db/connection");
+const musican = require("../db/routes/musician");
 const port = 3000;
 
-app.use(express.json())
-app.use(express.urlencoded())
 
 
-app.get("/musicians", async (req, res) => {
-    const musicians = await Musician.findAll();
-    res.json(musicians);
-  });
 
-app.get("/musicians/:id", async (req, res) => {
-    const id = req.params.id;
-    const musican = await Musician.findByPk(id);
-    res.json(musican);
-  });
+app.use("/musicians", musican);
 
+app.use("/musicians/:id", musican);
 
-app.post("/musicians", async (req, res, next) => {
-    
-    try{
-        const artist =  await Musician.create(req.body);
-        
+app.use("/musicians", musican);
 
-        res.status(201).json({message: "Musician Created"})
+app.use("/musicians/:id", musican);
 
-    }
-
-    catch (error) {
-        next (error)
-    }
-})
-
-app.put("/musicians/:id", async (req,res) => {
-
-    try{
-      await Musician.update(req.body, {
-        where: {id : req.params.id}
-      })
-      res.status(200)
-      }
-
-    catch (err)
-    {
-        res.status(404)
-    }
-})
-
-app.delete("/musicians/:id", async(req,res) => {
-    try{
-        await Musician.destroy({where: {id: req.params.id}})
-        res.status(200).message("User Deleted!")
-    }
-    catch(err){
-        res.send(400)
-    }
-
-})
-
-
+app.use("/musicians/:id", musican);
+  
 
 
 
